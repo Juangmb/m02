2389e3c39bcab7c3777a55f56448c40d1a52624c
Juangmb <juangmbm@gmail.com>
2022-02-01 23:11:58 +0100

    primera version de perro

---
+++ perro.py
@@ -0,0 +1,7 @@
+class Perro():
+    def __init__(self, nombre, edad, peso):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+    def ladrar():
+        print("Guau, guau")
